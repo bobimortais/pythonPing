545b92db958db785096eeac3a0b81c883fcbb4d4
Mad Hyena <robsonzacarias@hotmail.com>
2018-05-15 03:08:44 -0300

    Add files via upload

--- cgi-bin/TestPing.py
+++ cgi-bin/TestPing.py
@@ -1,1 +1,39 @@
+import os
 
+def printPingTime(data):
+	listPing = []
+	for linha in data:
+		if("TTL" in linha):
+			linhaParts = linha.split("tempo=")[1].split(" ")
+			listPing.append(str(linhaParts[0]))
+		elif("Request timed out" in linha):
+			listPing.append("Lost Package")
+		elif("General failure" in linha):
+			listPing.append("Connection closed")
+		
+	return listPing
+
+def getPing():			
+	os.remove('ping.txt')
+	os.system("ping google.com >> ping.txt")
+	data = open('ping.txt')
+	return data
+	
+listFinalPing = []	
+for x in range(1, 10):
+	data = getPing()	
+	listFinalPing.append(printPingTime(data))
+	data.close()
+ 
+print('Content-type: text/html\n\n')
+print('<html>')
+print('<head>')
+print('<title>Ping screen</title>')
+print('</head>')
+print('<body>')
+
+for value in listFinalPing:
+	print('<p>' + str(value) + '</p>')
+	
+print('</body>')
+print('</html>')
